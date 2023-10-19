df24694e447cb67e81b9f31409d18ebb000e5430
jjones <jimmy.jones@tutanota.com>
2023-10-19 17:32:27 -0600

    setup svg container

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
 	import { getContext } from 'svelte';
 
+	// grab the chart data from the store
 	const chartData: any = getContext('csvData');
+
+	// width, height and margins for svg container
+	const width: number = 960;
+	const height: number = 500;
+	const margin = { top: 20, right: 30, bottom: 50, left: 220 };
 </script>
 
-{JSON.stringify($chartData)}
+<svg {width} {height}>
+	<g transform={`translate(${margin.left}, ${margin.top})`} />
+</svg>
