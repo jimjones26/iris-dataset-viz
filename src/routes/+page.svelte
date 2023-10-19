119fb845c0e8aa9618d424c59e9bc406103ed803
jjones <jimmy.jones@tutanota.com>
2023-10-19 17:49:04 -0600

    add x and y values and scales

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,27 @@
 <script lang="ts">
 	import { getContext } from 'svelte';
+	import * as d3 from 'd3';
+	import type { IrisItem } from '$lib/stores/iris-interface';
 
 	// grab the chart data from the store
-	const chartData: any = getContext('csvData');
+	const chartData: Array<IrisItem> = getContext('csvData');
 
 	// width, height and margins for svg container
 	const width: number = 960;
 	const height: number = 500;
 	const margin = { top: 20, right: 30, bottom: 50, left: 220 };
+
+	// inner height and width of chart
+	const innerHeight = height - margin.top - margin.bottom;
+	const innerWidth = width - margin.left - margin.right;
+
+	// x and y axis values
+	const xValue = (item: IrisItem) => item.sepal_length;
+	const yValue = (item: IrisItem) => item.petal_width;
+
+	// x and y scales
+	const xScale = d3.scaleLinear().domain(d3.extent($chartData, xValue));
+	const yScale = d3.scaleLinear().domain(d3.extent($chartData, xValue)).range([0, innerHeight]);
 </script>
 
 <svg {width} {height}>
