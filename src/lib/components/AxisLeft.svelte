5957010163cce3755a99c7f5b661b55908f74b6b
jjones <jimmy.jones@tutanota.com>
2023-10-19 19:08:46 -0600

    fix y scale to use ticks instead of domain

--- src/lib/components/AxisLeft.svelte
+++ src/lib/components/AxisLeft.svelte
@@ -2,6 +2,6 @@
 	export let yScale: any;
 </script>
 
-{#each yScale.domain() as tick, i}
+{#each yScale.ticks() as tick, i}
 	<text fill="#635F5D" style="text-anchor: end;" dy=".32em" x={-3} y={yScale(tick)}>{tick}</text>
 {/each}
